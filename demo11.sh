89298f585d2ce2113b4fa4c22d64baf26d074636
dac <dac@students.acts.in>
2024-12-21 16:17:42 +0530

    3rd commit

--- demo11.sh
+++ demo11.sh
@@ -3,5 +3,6 @@
 echo $$                
 read -p "Enter your num1: " num1
 read -p "Enter your num2: " num2
-sum=$((num1 + num2))
+read -p "Enter your num3: " num3
+sum=$((num1 + num2 + num3))
 echo "The sum of $num1 and $num2 is $sum"
